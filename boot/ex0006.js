0d8374873d5934d97757a307361e698714893b0d
Luiz Medina <luizmedina87@gmail.com>
2026-04-14 14:46:59 -0400

    Great new exercise on the call stack and task queue

---
+++ boot/ex0006.js
@@ -0,0 +1,25 @@
+async function processAnalytics(data) {
+  let analysis = "";
+
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(analysis);
+    }, 100);
+
+    setTimeout(() => {
+      analysis += " - Finished!";
+    }, 0);
+
+    // don't touch above this line
+
+    Promise.resolve().then(() => (analysis += `- Processing: ${data}`));
+
+    // don't touch below this line
+
+    analysis += "Analyzing...";
+  });
+}
+
+processAnalytics("Excuse me can I talk to you for a minute").then((analysis) =>
+  console.log(analysis),
+);
